be17e739e66c844f421fe943f065762045bcd2f8
Royer Blackberry <shadowblack@outlook.com>
2025-11-26 19:06:16 -0600

    modificaciones

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ MAIN_TEMPLATE = '''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Juego del Impostor - Royer Blackberry</title>
+    <title>Juego del Underconver - Royer Blackberry</title>
     <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
         body {
